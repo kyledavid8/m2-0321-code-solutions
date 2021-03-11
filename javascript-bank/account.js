bf74e887e11c2ed522f0dedf3c7780d32c69b4b7
Kyle Cunningham <kyle8david@gmail.com>
2021-03-11 14:38:43 -0700

    completed javascript-bank

---
+++ javascript-bank/account.js
@@ -0,0 +1,38 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function(amount) {
+  if (amount > 0) {
+    this.transactions.push(new Transaction('deposit', amount));
+    return true;
+  } else {
+    this.transactions.push(new Transaction('deposit', 0));
+    return false;
+  }
+}
+
+Account.prototype.withdraw = function(amount) {
+  if (amount > 0) {
+    this.transactions.push(new Transaction('withdraw', amount));
+    return true;
+  } else {
+    this.transactions.push(new Transaction('withdraw', 0));
+    return false;
+  }
+}
+
+Account.prototype.getBalance = function() {
+  var balance = 0;
+  for (var counter = 0; this.transactions.length > counter; counter++) {
+    if (this.transactions[counter].type === 'deposit') {
+      balance += this.transactions[counter].amount;
+    } else {
+      balance -= this.transactions[counter].amount;
+    }
+  }
+  return balance;
+}
