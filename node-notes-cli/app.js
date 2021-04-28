da179be40cd2abd43f8d673c91166f77134bccdc
Kyle Cunningham <kyle8david@gmail.com>
2021-04-27 19:26:37 -0600

    completed node notes-cli

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,31 +1,36 @@
-const dataObject = require('./data.json')
 const fs = require('fs');
 
 
 fs.readFile('data.json', 'utf8', (err, data) => {
-  data = JSON.parse(data);
-  if(process.argv[2] === 'read') {
+  if(process.argv[2] !== 'read') {
+    if(process.argv[3] === undefined) {
+      console.log('Another parameter is needed');
+    } else {
+      data = JSON.parse(data);
+      if (process.argv[2] === 'create') {
+        let nextId = data.nextId;
+        data.notes[nextId] = process.argv[3];
+        data.nextId++;
+      } else if (process.argv[2] === 'delete') {
+        delete data.notes[process.argv[3].toString()];
+      } else if (process.argv[2] === 'update') {
+        data.notes[process.argv[3]] = process.argv[4];
+      }
+      data = JSON.stringify(data);
+      fs.writeFile('data.json', data, (err) => {
+        if (err) throw err;
+      })
+    }
+  } else if(process.argv[2] === 'read') {
+    data = JSON.parse(data);
     for (var i = 1; data.nextId > i; i++) {
-      console.log(`${i}: ${data.notes[i.toString()]}`);
+      if(data.notes[i] !== undefined) {
+        console.log(`${i}: ${data.notes[i.toString()]}`);
+      }
     }
-  } else if(process.argv[2] === 'create') {
-    let nextId = data.nextId;
-    data.notes[nextId] = process.argv[3];
-    data.nextId++;
-  } else if(process.argv[2] === 'delete') {
-    delete data.notes[process.argv[3]];
-    data.nextId--;
-    let counter = parseInt(process.argv[3]) + 1;
-    for (var i = process.argv[3]; data.nextId > i; i++) {
-      data.notes[i] = data.notes[counter];
-      counter++;
-    }
-    delete data.notes[data.nextId];
-  } else if(process.argv[2] === 'update') {
-    data.notes[process.argv[3]] = process.argv[4];
+    data = JSON.stringify(data);
+    fs.writeFile('data.json', data, (err) => {
+      if (err) throw err;
+    })
   }
-  data = JSON.stringify(data);
-  fs.writeFile('data.json', data, (err) => {
-    if (err) throw err;
-  })
 })
